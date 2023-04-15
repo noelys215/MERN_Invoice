caf6ceeea49d913df3680641ba0d556467309d00
Henry B <noelys215@gmail.com>
2023-04-14 22:33:09 -0400

    FEAT: Add logging w/Winston & Morgan

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
+import { morganMiddleware, systemLogs } from './utils/Logger.js';
 
 const app = express();
 
@@ -14,14 +15,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 
+app.use(morganMiddleware);
+
 app.get('/api/v1/test', (req, res) => res.json({ Hi: 'Welcome to the invoice app.' }));
 
 const PORT = process.env.PORT || 1997;
 
-app.listen(PORT, () =>
+app.listen(PORT, () => {
 	console.log(
-		`${chalk.green.bold('✓')} 👍 Server running in ${chalk.yellow.bold(
+		`${chalk.green.bold('✔')} 👍 Server running in ${chalk.yellow.bold(
 			process.env.NODE_ENV
-		)} on port ${chalk.blue.bold(PORT)}`
-	)
-);
+		)} mode on port ${chalk.blue.bold(PORT)}`
+	);
+	systemLogs.info(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
