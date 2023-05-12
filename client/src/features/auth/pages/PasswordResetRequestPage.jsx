cf087322785907b4be1e0c8f8454b0c9f74eaaa1
Henry B <noelys215@gmail.com>
2023-05-11 21:09:26 -0400

    FEAT: Add PasswordResetRequestPage.jsx

---
+++ client/src/features/auth/pages/PasswordResetRequestPage.jsx
@@ -0,0 +1,178 @@
+import SendIcon from '@mui/icons-material/Send';
+import {
+	Box,
+	Button,
+	Container,
+	FormHelperText,
+	Grid,
+	InputLabel,
+	OutlinedInput,
+	Stack,
+	Typography,
+} from '@mui/material';
+import { Formik } from 'formik';
+import { useEffect } from 'react';
+import { GoMailRead } from 'react-icons/go';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import * as Yup from 'yup';
+import { Spinner } from '../../../components/Spinner';
+import { StyledDivider } from '../../../components/StyledDivider';
+import { useTitle } from '../../../hooks/useTitle';
+import { AuthWrapper } from '../forms/AuthWrapper';
+import { usePasswordResetRequestMutation } from '../authApiSlice';
+
+export const PasswordResetRequestPage = () => {
+	useTitle('Request Reset Password');
+	/* Hooks */
+	const navigate = useNavigate();
+	const goBack = () => navigate(-1);
+
+	const [passwordResetRequest, { data, isLoading, isSuccess }] =
+		usePasswordResetRequestMutation();
+
+	useEffect(() => {
+		if (isSuccess) {
+			navigate('/login');
+			const message = data.message;
+			toast.success(message);
+		}
+	}, [data, isSuccess, navigate]);
+
+	/* HANDLERS */
+	const handleSubmit = async (values, { setStatus, setSubmitting }) => {
+		try {
+			await passwordResetRequest(values).unwrap();
+			setStatus({ success: true });
+			setSubmitting(false);
+		} catch (err) {
+			const message = err.data.message;
+			toast.error(message);
+			setStatus({ success: false });
+			setSubmitting(false);
+		}
+	};
+
+	return (
+		<>
+			<Formik
+				initialValues={{ email: '' }}
+				validationSchema={Yup.object().shape({
+					email: Yup.string()
+						.email('Must be a valid email')
+						.max(255)
+						.required('Email is required'),
+				})}
+				onSubmit={handleSubmit}>
+				{({
+					errors,
+					handleBlur,
+					handleChange,
+					handleSubmit,
+					isSubmitting,
+					touched,
+					values,
+				}) => (
+					<AuthWrapper>
+						<Container
+							component="main"
+							maxWidth="sm"
+							sx={{
+								border: '2px solid  #e4e5e7',
+								borderRadius: '25px',
+								py: 2,
+							}}>
+							<form noValidate autoComplete="off" onSubmit={handleSubmit}>
+								<Grid>
+									<Grid item xs={12}>
+										<Box
+											sx={{
+												display: 'flex',
+												flexDirection: 'row',
+												justifyContent: 'center',
+												alignItems: 'center',
+											}}>
+											{' '}
+											<GoMailRead className="auth-svg" />
+											<Typography variant="h2">Enter Your Email</Typography>
+										</Box>
+										<StyledDivider />
+									</Grid>
+									<Box
+										sx={{
+											display: 'flex',
+											flexDirection: 'row',
+											justifyContent: 'center',
+											alignItems: 'center',
+											marginBottom: '20px',
+										}}>
+										<Typography variant="body1" component="div">
+											Are you sure you want to reset your password?
+										</Typography>
+									</Box>
+								</Grid>
+								{isLoading ? (
+									<Spinner />
+								) : (
+									<Grid container>
+										{/* EMAIL */}
+										<Grid item xs={12}>
+											<Stack spacing={1}>
+												<InputLabel htmlFor="email-signup">
+													Email Address*
+												</InputLabel>
+												<OutlinedInput
+													fullWidth
+													error={Boolean(touched.email && errors.email)}
+													id="email-signup"
+													type="email"
+													value={values.email}
+													name="email"
+													onBlur={handleBlur}
+													onChange={handleChange}
+													placeholder="email@example.com"
+													inputProps={{}}
+												/>
+												{touched.email && errors.email && (
+													<FormHelperText
+														error
+														id="helper-text-email-signup">
+														{errors.email}
+													</FormHelperText>
+												)}
+											</Stack>
+										</Grid>
+										{/* BUTTON */}
+										<Grid item xs={12}>
+											<Button
+												sx={{ mt: 3, mb: 2 }}
+												type="submit"
+												fullWidth
+												variant="contained"
+												color="success"
+												size="large"
+												endIcon={<SendIcon />}
+												disabled={!values.email}>
+												Send Password Reset Email
+											</Button>
+										</Grid>
+										{/* GO BACK */}
+										<Grid item xs={12}>
+											<Button
+												variant="contained"
+												color="warning"
+												size="medium"
+												onClick={goBack}>
+												Go Back
+											</Button>
+										</Grid>
+									</Grid>
+								)}
+							</form>
+						</Container>
+					</AuthWrapper>
+				)}
+			</Formik>
+		</>
+	);
+};
