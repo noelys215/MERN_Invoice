39799420f4345528331d23e1748ed01c25b48126
Henry B <noelys215@gmail.com>
2023-05-12 19:46:57 -0400

    FEAT: Add AuthRequired.jsx & roles.js

---
+++ client/src/components/AuthRequired.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuthUser } from '../hooks/useAuthUser';
+
+export const AuthRequired = ({ allowedRoles }) => {
+	const location = useLocation();
+	const { roles } = useAuthUser();
+
+	return roles.some((role) => allowedRoles.includes(role)) ? (
+		<Outlet />
+	) : (
+		<Navigate to="/login" state={{ from: location }} replace />
+	);
+};
