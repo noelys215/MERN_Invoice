288657332f4d0dd2fddf106db7236a14d99ab17f
Henry B <noelys215@gmail.com>
2023-06-11 10:36:01 -0400

    FEAT: Add NormalDivider.jsx

---
+++ client/src/components/NormalDivider.jsx
@@ -0,0 +1,10 @@
+import { Divider, styled } from '@mui/material';
+
+const DividerStyle = styled(Divider)({
+	height: '3px',
+	backgroundColor: 'rgb(17,65,141)',
+	marginBottom: '20px',
+	marginTop: '25px',
+});
+
+export const NormalDivider = () => <DividerStyle />;
