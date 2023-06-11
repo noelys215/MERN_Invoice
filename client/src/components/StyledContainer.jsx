02f88e3db2d7454aa30e2e9f85e39c977d056941
Henry B <noelys215@gmail.com>
2023-06-11 10:54:28 -0400

    FEAT: Add StyledContainer.jsx

---
+++ client/src/components/StyledContainer.jsx
@@ -0,0 +1,11 @@
+import { Box, styled } from '@mui/material';
+
+const StyledBox = styled(Box)({
+	width: '100%',
+	margin: '20px auto 100px auto',
+	borderRadius: '10px',
+	padding: '20px',
+	boxShadow: '0 1px 3px rgba(0,0,0,0.3), 0 1px 2px rgba(0,0,0,0.6)',
+});
+
+export const StyledContainer = ({ children }) => <StyledBox>{children}</StyledBox>;
