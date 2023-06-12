9d89089371d743b7c4986e70f3f6ea80857f46e1
Henry B <noelys215@gmail.com>
2023-06-12 23:00:00 +0300

    FEAT: Add StyledDashboardGrid.jsx

---
+++ client/src/components/StyledDashboardGrid.jsx
@@ -0,0 +1,20 @@
+import { Box, Grid, styled } from '@mui/material';
+
+const StyledBox = styled(Box)({
+	width: '100%',
+	marginTop: '20px',
+	marginLeft: 'auto',
+	marginRight: 'auto',
+	borderRadius: '10px',
+	padding: '20px',
+	border: '1px dashed #5a5a5a',
+	borderWidth: '2px',
+});
+
+export const StyledDashboardGrid = ({ children }) => {
+	return (
+		<Grid item md={3}>
+			<StyledBox>{children}</StyledBox>
+		</Grid>
+	);
+};
