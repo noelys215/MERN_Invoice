0dc1f5cf0ac2f2d66db924190a0441da80598434
Henry B <noelys215@gmail.com>
2023-06-16 15:36:10 -0400

    FEAT: Finish DockCreateEditForm.jsx

--- client/src/features/documents/pages/DocCreateEditForm.jsx
+++ client/src/features/documents/pages/DocCreateEditForm.jsx
@@ -209,8 +209,520 @@ export const DocCreateEditForm = () => {
 		}
 	};
 	return (
-		<div>
-			<></>
-		</div>
+		<Container component="main" maxWidth="lg" sx={{ mt: 10 }}>
+			<CssBaseline />
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}>
+				<NoteAddIcon sx={{ fontSize: 70 }} />
+				<Typography variant="h2">Create Document</Typography>
+				<Button
+					variant="contained"
+					color="warning"
+					size="small"
+					sx={{ fontSize: '1rem', ml: '10px' }}
+					onClick={goBack}>
+					Go Back
+				</Button>
+			</Box>
+			<StyledDivider />
+			{isLoading || updateDocLoading ? (
+				<Spinner />
+			) : (
+				<Box
+					sx={{
+						mt: '1rem',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+					component="form"
+					noValidate
+					autoComplete="off"
+					onSubmit={createUpdateDocHandler}>
+					<StyledContainer>
+						<Grid container justifyContent="space-between">
+							<Grid></Grid>
+							<Grid>
+								<DocumentType
+									documentType={documentType}
+									setDocumentType={setDocumentType}
+								/>
+							</Grid>
+						</Grid>
+						<NormalDivider />
+						<Grid container justifyContent="space-between" sx={{ mt: '30px' }}>
+							<Grid item sx={{ width: '50%' }}>
+								<Container>
+									<Typography
+										variant="inherit"
+										style={{
+											color: '#5a5a5a',
+											pl: '3px',
+											textTransform: 'uppercase',
+										}}
+										gutterBottom>
+										Send to:
+									</Typography>
+									{customer && (
+										<>
+											<Typography variant="subtitle1" gutterBottom>
+												<b>Name:</b> {customer?.name}
+											</Typography>
+											<Typography variant="body1" gutterBottom>
+												<b>Email:</b> {customer?.email}
+											</Typography>
+											<Typography variant="body1" gutterBottom>
+												<b>AccountNo:</b> {customer?.accountNo}
+											</Typography>
+											<Typography variant="body1" gutterBottom>
+												<b>VAT/TIN No:</b> {customer?.vatTinNo}
+											</Typography>
+											<Typography variant="body1">
+												<b>Phone Number:</b> {customer?.phoneNumber}
+											</Typography>
+											<Typography variant="body1">
+												<b>Address:</b> {customer?.address}
+											</Typography>
+											<Button
+												sx={{ textTransform: 'none' }}
+												color="warning"
+												size="large"
+												onClick={() => setCustomer(null)}
+												startIcon={<ChangeCircleIcon color="warning" />}>
+												choose another customer
+											</Button>
+										</>
+									)}
+									<div
+										style={
+											customer ? { display: 'none' } : { display: 'block' }
+										}>
+										<Autocomplete
+											disablePortal
+											sx={{ pt: '10px' }}
+											id="customers-list"
+											options={customers?.myCustomers || []}
+											getOptionLabel={(option) => (option ? option.name : '')}
+											renderInput={(params) => (
+												<TextField {...params} label="Select a customer" />
+											)}
+											value={customers?.myCustomers?.name}
+											onChange={(event, value) => {
+												setCustomer(value);
+											}}
+										/>
+									</div>
+
+									{!customer && (
+										<>
+											<Grid item sx={{ pt: '10px', pb: '10px' }}>
+												<Chip
+													color="secondary"
+													icon={<FaceIcon />}
+													label="Add New Customer"
+													onClick={() => navigate('/create-customer')}
+												/>
+											</Grid>
+										</>
+									)}
+								</Container>
+							</Grid>
+
+							<Grid item style={{ marginRight: 20, textAlign: 'right' }}>
+								<Typography
+									sx={{
+										textTransform: 'uppercase',
+										color: '#5a5a5a',
+									}}
+									gutterBottom>
+									Payment Status
+								</Typography>
+
+								<Typography
+									variant="h5"
+									style={{
+										color: documentType === 'Receipt' ? 'green' : 'red',
+									}}
+									gutterBottom>
+									{documentType === 'Receipt' ? 'Paid' : 'Not Paid'}
+								</Typography>
+
+								<Typography
+									sx={{
+										display: 'flex',
+										textTransform: 'uppercase',
+										color: '#5a5a5a',
+									}}
+									gutterBottom>
+									<CalendarMonthIcon
+										sx={{ alignItems: 'center' }}
+										fontSize="small"
+										color="info"
+									/>
+									Date of Issue:
+								</Typography>
+								<Typography gutterBottom>
+									<b>{format(new Date(), 'do MMMM yyyy')}</b>
+								</Typography>
+
+								<Typography
+									sx={{
+										display: 'flex',
+										textTransform: 'uppercase',
+										color: '#5a5a5a',
+									}}
+									gutterBottom>
+									<CalendarMonthIcon fontSize="small" color="warning" />
+									Due Date:
+								</Typography>
+								<Typography variant="body1" gutterBottom>
+									<b>{dueDate && format(dueDate, 'do MMMM yyyy')}</b>
+								</Typography>
+
+								<Typography
+									sx={{
+										display: 'flex',
+										textTransform: 'uppercase',
+										color: '#5a5a5a',
+									}}
+									gutterBottom>
+									<CurrencyExchangeIcon fontSize="small" color="success" />
+									Total Amount:{' '}
+								</Typography>
+								<Typography variant="h6" gutterBottom>
+									{currency}
+									{addCurrencyCommas(total.toFixed(2))}
+								</Typography>
+							</Grid>
+						</Grid>
+
+						<NormalDivider />
+
+						<div>
+							<TableContainer component={Paper} sx={{ marginBottom: '100px' }}>
+								<Table sx={{ minWidth: 700 }} aria-label="simple-table">
+									<TableHead>
+										<TableRow>
+											<StyledTableCell>#</StyledTableCell>
+											<StyledTableCell>Product</StyledTableCell>
+											<StyledTableCell>Qty</StyledTableCell>
+											<StyledTableCell>Unit Price</StyledTableCell>
+											<StyledTableCell>Disc(%)</StyledTableCell>
+											<StyledTableCell>Line Total</StyledTableCell>
+											<StyledTableCell>Remove</StyledTableCell>
+										</TableRow>
+									</TableHead>
+									<TableBody>
+										{items.map((item, index) => (
+											<StyledTableRow key={index}>
+												<StyledTableCell component="th" scope="row">
+													{index + 1}
+												</StyledTableCell>
+												<StyledTableCell style={{ width: '40%' }}>
+													<InputBase
+														multiline
+														style={{
+															width: '100%',
+														}}
+														outline="none"
+														sx={{ ml: 1, flex: 1 }}
+														type="text"
+														onChange={(e) => {
+															const itemName = e.target.value;
+															setItems((currentItem) =>
+																produce(currentItem, (v) => {
+																	v[index].itemName = itemName;
+																})
+															);
+														}}
+														value={item.itemName}
+														placeholder="Name/Description"
+													/>
+												</StyledTableCell>
+												{/* quantity */}
+												<StyledTableCell align="right">
+													<InputBase
+														sx={{ ml: 1, flex: 1 }}
+														type="number"
+														onChange={(e) => {
+															const quantity = e.target.value;
+															setItems((currentItem) =>
+																produce(currentItem, (v) => {
+																	v[index].quantity = quantity;
+																})
+															);
+														}}
+														value={item.quantity}
+														placeholder="0"
+													/>
+												</StyledTableCell>
+												{/* unit price */}
+												<StyledTableCell align="right">
+													<InputBase
+														sx={{ ml: 1, flex: 1 }}
+														type="number"
+														onChange={(e) => {
+															const unitPrice = e.target.value;
+															setItems((currentItem) =>
+																produce(currentItem, (v) => {
+																	v[index].unitPrice = unitPrice;
+																})
+															);
+														}}
+														value={item.unitPrice}
+														placeholder="0"
+													/>
+												</StyledTableCell>
+
+												{/* discount */}
+												<StyledTableCell align="right">
+													<InputBase
+														sx={{ ml: 1, flex: 1 }}
+														type="number"
+														onChange={(e) => {
+															const discount = e.target.value;
+															setItems((currentItem) =>
+																produce(currentItem, (v) => {
+																	v[index].discount = discount;
+																})
+															);
+														}}
+														value={item.discount}
+														placeholder="0"
+													/>
+												</StyledTableCell>
+
+												{/* line total */}
+												<StyledTableCell align="right">
+													<InputBase
+														sx={{ ml: 1, flex: 1 }}
+														disabled
+														type="number"
+														name="amount"
+														value={(
+															item?.quantity * item.unitPrice -
+															(item.quantity *
+																item.unitPrice *
+																item.discount) /
+																100
+														).toFixed(2)}
+													/>
+												</StyledTableCell>
+
+												<StyledTableCell align="right">
+													<IconButton
+														onClick={() => {
+															setItems(
+																items.filter(
+																	(i) =>
+																		i.itemName !== item.itemName
+																)
+															);
+															setSubTotal(0);
+															setTotal(0);
+															setSalesTax(0);
+														}}>
+														<DeleteForeverIcon
+															style={{
+																width: '20px',
+																height: '20px',
+															}}
+															color="error"
+														/>
+													</IconButton>
+												</StyledTableCell>
+											</StyledTableRow>
+										))}
+									</TableBody>
+								</Table>
+							</TableContainer>
+							<StyledItemButton
+								className="new-customer-btn"
+								variant="contained"
+								color="success"
+								startIcon={<AddCircleOutlineIcon />}
+								onClick={handleAddBillingItemsRow}>
+								Add Product or Service
+							</StyledItemButton>
+						</div>
+
+						<Box
+							sx={{
+								marginLeft: '50%',
+								textAlign: 'left',
+								borderBottom: '1px solid rgb(17,65,141)',
+							}}>
+							<Typography variant="h6" className="title">
+								Cost Summary
+							</Typography>
+							<div className="billItem">
+								<Typography variant="subtitle1">Sub total:</Typography>
+								<h4>
+									{currency} {subTotal.toFixed(2)}
+								</h4>
+							</div>
+
+							<div className="billItem">
+								<Typography variant="subtitle1">VAT/Sales Tax:</Typography>
+								<h4>{salesTax.toFixed(1)}</h4>
+							</div>
+
+							<div className="billItem">
+								<Typography variant="subtitle1">Total:</Typography>
+								<h4
+									style={{
+										color: 'black',
+										fontSize: '18px',
+										lineHeight: '8px',
+									}}>
+									{currency}
+									{addCurrencyCommas(total.toFixed(2))}
+								</h4>
+							</div>
+						</Box>
+
+						<div className="toolbar">
+							<Container>
+								<Grid container>
+									<Grid
+										item
+										sx={{
+											marginTop: '16px',
+											marginRight: 5,
+										}}>
+										<TextField
+											type="text"
+											step="any"
+											name="rates"
+											id="rates"
+											value={rates}
+											onChange={handleRates}
+											placeholder="such as 18% etc"
+											label="VAT(%)"
+										/>
+									</Grid>
+									<Grid
+										item
+										sx={{
+											marginTop: '16px',
+											marginRight: 5,
+										}}>
+										<LocalizationProvider dateAdapter={AdapterDateFns}>
+											<DatePicker
+												label="Set Due Date"
+												value={dueDate}
+												onChange={(date) => {
+													setDueDate(date);
+												}}
+												renderInput={(params) => <TextField {...params} />}
+											/>
+										</LocalizationProvider>
+									</Grid>
+
+									<Grid item sx={{ width: 255, marginTop: '5px' }}>
+										<Autocomplete
+											disablePortal
+											sx={{ pt: '10px' }}
+											id="currency-list"
+											options={currencies}
+											getOptionLabel={(option) =>
+												option ? option.currency : ''
+											}
+											renderInput={(params) => (
+												<TextField
+													{...params}
+													label="Select your currency"
+												/>
+											)}
+											value={currencies.code}
+											onChange={(e, value) => setCurrency(value.code)}
+										/>
+									</Grid>
+								</Grid>
+							</Container>
+						</div>
+
+						<Box
+							sx={{
+								marginTop: '20px',
+								display: 'flex',
+								flexDirection: 'row',
+								justifyContent: 'space-between',
+							}}>
+							<Box item>
+								<Typography variant="h4" sx={{ color: 'rgb(17,65,141)' }}>
+									Additional Info
+								</Typography>
+								<NormalDivider />
+								<TextareaAutosize
+									minRows={4}
+									style={{
+										width: 350,
+										border: 'solid 1px #d6d6d6',
+										padding: '10px',
+									}}
+									placeholder="Add a special note or memo to your customers,such as payment information/account"
+									onChange={(e) =>
+										setDocData({
+											...docData,
+											additionalInfo: e.target.value,
+										})
+									}
+									value={docData.additionalInfo}
+								/>
+							</Box>
+
+							<Box>
+								<Typography variant="h4" sx={{ color: 'rgb(17,65,141)' }}>
+									Terms & Conditions
+								</Typography>
+								<NormalDivider />
+								<TextareaAutosize
+									minRows={4}
+									style={{
+										width: 350,
+										border: 'solid 1px #d6d6d6',
+										padding: '10px',
+									}}
+									placeholder="Add legal terms or conditions, such as your return/refund policy, shipping info, product warranties or privacy policy"
+									onChange={(e) =>
+										setDocData({
+											...docData,
+											termsConditions: e.target.value,
+										})
+									}
+									value={docData.termsConditions}
+								/>
+							</Box>
+						</Box>
+
+						<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+							<Button
+								variant="outlined"
+								type="submit"
+								size="large"
+								sx={{
+									marginTop: '20px',
+									borderColor: 'rgb(17,65,141)',
+									borderRadius: '30px',
+									'&:hover': {
+										bgcolor: 'rgb(17,65,141)',
+										color: 'white',
+										borderColor: 'rgb(17,65,141)',
+									},
+								}}
+								startIcon={<SaveAsIcon />}>
+								Create/Update Doc
+							</Button>
+						</Box>
+					</StyledContainer>
+				</Box>
+			)}
+		</Container>
 	);
 };
